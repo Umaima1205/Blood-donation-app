86aec4d7ed7236c736e5fce258908faddfe5e5c8
Umaima Yousuf <112200777+Umaima1205@users.noreply.github.com>
2023-10-24 00:06:31 +0500

    A blood donation built with flutter
    A blood donation app built with flutter

--- zindgiapp/android/app/src/main/kotlin/com/example/zindgiapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.zindgiapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- zindgiapp/lib/utility/App_Colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  // ignore: constant_identifier_names
-  static const RedColor = Color(0xffEB3738);
-  static const WhiteColor = Color(0xffFFFFFF);
-
-}
